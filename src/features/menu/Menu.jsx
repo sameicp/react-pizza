92b7105a8d8ec84d00b49e5763e7eb0721bfa04b
sameicp <same.eth2001@gmail.com>
2024-04-22 23:27:22 +0200

    inital commit

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import MenuItem from "./MenuItem.jsx"
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+
+function Menu() {
+  const menu = useLoaderData()
+  console.log(menu)
+  return (
+    <ul>
+      {menu.map(pizza => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  )
+}
+
+export async function loader() {
+  const menu = await getMenu()
+  return menu
+}
+
+export default Menu;
